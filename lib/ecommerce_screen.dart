5e529ee592a572a6d3a0dcb3f1903e589c5d6419
vdinhthien <blv.tu97@gmail.com>
2023-06-22 14:11:27 +0700

    Fix bug

--- lib/ecommerce_screen.dart
+++ lib/ecommerce_screen.dart
@@ -19,58 +19,35 @@ class EcommerceScreen extends StatelessWidget {
             // Adjust the height of the overlapping container as needed
             child: Container(
               color: const Color(0xFF175cd3),
-              child: const Row(
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  SizedBox(width: 12),
-                  SizedBox(
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back_ios,
+                        color: Color(0xFF475467)),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  const SizedBox(width: 12),
+                  const SizedBox(
                     child: Image(
                       image: AssetImage("assets/soctrip_logo.png"),
                       width: 35,
                       height: 35,
                     ),
                   ),
-                  SizedBox(width: 6),
-                  Text(
-                    'Soctrip',
+                  const SizedBox(width: 6),
+                  const Text(
+                    'Ecommerce App',
                     style: TextStyle(
                       fontSize: 24,
                       color: Colors.white,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  Spacer(),
+                  const Spacer(),
                 ],
-              ),
-            ),
-          ),
-          Positioned(
-            top: 100,
-            left: 0,
-            right: 0,
-            height: 70,
-            // Adjust the height of the overlapping container as needed
-            child: Container(
-              margin: const EdgeInsets.only(left: 12, right: 12),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(16)),
-                boxShadow: <BoxShadow>[
-                  BoxShadow(
-                    color: Color.fromRGBO(16, 24, 40, 0.05),
-                    offset: Offset(0, 1),
-                    blurRadius: 2,
-                    spreadRadius: 0,
-                  )
-                ],
-              ),
-              child: const Text(
-                'Ecommerce App',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Color(0xFF175cd3),
-                  fontWeight: FontWeight.bold,
-                ),
               ),
             ),
           ),
